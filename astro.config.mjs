70b2afe7c76b16dce370d4d55e0917522e6feb89
Galaxie <Galaxie_ops@outlook.com>
2025-08-15 16:56:25 +0800

    添加数据结构笔记部分

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,47 +6,61 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
     site: 'https://GalaxieLRY.github.io',
     base: 'NoteWeb',
-	integrations: [
-		starlight({
-			title: '首页',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/GalaxieLRY/NoteWeb' }],
+    integrations: [
+        starlight({
+            title: '首页',
+            social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/GalaxieLRY/NoteWeb' }],
             locales: {
-				root: {
-					label: '简体中文',
-					lang: 'zh'
-				}
-      		},
-			sidebar: [
-				{ label: '简介', link: '/intro/' },
+                root: {
+                    label: '简体中文',
+                    lang: 'zh'
+                }
+            },
+            sidebar: [
+                { label: '简介', link: '/intro/' },
                 {
-					label: 'Astro样式指南',
-                    autogenerate: { directory:'astro' },
-					// items: [
-					// 	// Each item here is one entry in the navigation menu.
-					// 	{ label: 'prompt', slug: 'llm/prompt' },
-					// ],
-				},
+                    label: 'Astro样式指南',
+                    autogenerate: { directory: 'astro' },
+                    // items: [
+                    // 	// Each item here is one entry in the navigation menu.
+                    // 	{ label: 'prompt', slug: 'llm/prompt' },
+                    // ],
+                },
                 {
-					label: 'LLM',
-                    autogenerate: { directory:'LLM' },
-					// items: [
-					// 	// Each item here is one entry in the navigation menu.
-					// 	{ label: 'prompt', slug: 'llm/prompt' },
-					// ],
-				},
+                    label: 'LLM',
+                    autogenerate: { directory: 'LLM' },
+                    // items: [
+                    // 	// Each item here is one entry in the navigation menu.
+                    // 	{ label: 'prompt', slug: 'llm/prompt' },
+                    // ],
+                },
                 {
-					label: '刷题题解',
-                    autogenerate: { directory:'keyToAlgoExercises' },
-				},
+                    label: '刷题题解',
+                    autogenerate: { directory: 'keyToAlgoExercises' },
+                },
                 {
-					label: '前端开发',
-                    autogenerate: { directory:'front' },
-				},
+                    label: '前端开发',
+                    autogenerate: { directory: 'front' },
+                },
                 {
-					label: '杂项',
-                    autogenerate: { directory:'misc' },
-				},
-			],
-		}),
-	],
+                    label: '数据结构',
+                    // autogenerate: { directory: 'datastructure' },
+                    items: [
+                        { label: '注意事项', slug: 'datastructure/注意事项' },
+                        { label: '第1章 绪论', slug: 'datastructure/第1章_绪论' },
+                        { label: '第2章 线性表', slug: 'datastructure/第2章_线性表' },
+                        { label: '第3章 栈和队列', slug: 'datastructure/第3章_栈和队列' },
+                        { label: '第6章 树和二叉树', slug: 'datastructure/第6章_树和二叉树' },
+                        { label: '第7章 图', slug: 'datastructure/第7章_图' },
+                        { label: '第9章 查找', slug: 'datastructure/第9章_查找' },
+                        { label: '第10章 排序', slug: 'datastructure/第10章_排序' },
+                    ]
+                },
+                {
+                    label: '杂项',
+                    autogenerate: { directory: 'misc' },
+                },
+            ],
+        }),
+    ],
 });
