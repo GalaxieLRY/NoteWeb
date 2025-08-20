2da0c7b2a37ad367afc0d5705589ceb6e8d19f8b
Galaxie <Galaxie_ops@outlook.com>
2025-08-20 10:42:17 +0800

    添加【哈希】类型的题的题解

--- astro.config.mjs
+++ astro.config.mjs
@@ -23,24 +23,12 @@ export default defineConfig({
             sidebar: [
                 { label: '简介', link: '/intro/' },
                 {
-                    label: 'Astro样式指南',
-                    autogenerate: { directory: 'astro' },
-                    // items: [
-                    // 	// Each item here is one entry in the navigation menu.
-                    // 	{ label: 'prompt', slug: 'llm/prompt' },
-                    // ],
-                },
-                {
                     label: 'LLM',
                     autogenerate: { directory: 'LLM' },
                     // items: [
                     // 	// Each item here is one entry in the navigation menu.
                     // 	{ label: 'prompt', slug: 'llm/prompt' },
                     // ],
-                },
-                {
-                    label: '刷题题解',
-                    autogenerate: { directory: 'keyToAlgoExercises' },
                 },
                 {
                     label: '前端开发',
@@ -61,8 +49,20 @@ export default defineConfig({
                     ]
                 },
                 {
+                    label: '刷题题解',
+                    autogenerate: { directory: 'keyToAlgoExercises' },
+                },
+                {
                     label: '杂项',
                     autogenerate: { directory: 'misc' },
+                },
+                {
+                    label: 'Astro样式指南',
+                    autogenerate: { directory: 'astro' },
+                    // items: [
+                    // 	// Each item here is one entry in the navigation menu.
+                    // 	{ label: 'prompt', slug: 'llm/prompt' },
+                    // ],
                 },
             ],
         }),
